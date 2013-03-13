ac0c87b5be0e55a7e59bee387ce55c02d6c1079d
Christopher Orr <chris@orr.me.uk>
2013-03-13 21:34:41 +0100

    Update for German and ridiculous product names.

--- Source/content_script.js
+++ Source/content_script.js
@@ -35,7 +35,17 @@ function handleText(textNode)
 	v = v.replace(/\bThe cloud\b/g, "My butt");
 	v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+
+	// German + Telekom
+	v = v.replace(/\bDie (Telekom)?Cloud\b/g, "Mein Arsch");
+	v = v.replace(/\bDer (Telekom)?Cloud\b/g, "Meinem Arsch");
+	v = v.replace(/\bZur (Telekom)?Cloud\b/g, "Zu meinem Arsch");
+	v = v.replace(/\bdie (Telekom)?Cloud\b/g, "mein Arsch");
+	v = v.replace(/\bder (Telekom)?Cloud\b/g, "meinem Arsch");
+	v = v.replace(/\bzur (Telekom)?Cloud\b/g, "zu meinem Arsch");
+	v = v.replace(/Telekom ?Cloud/g, "TelekomArsch");
+	v = v.replace(/Mail & Cloud/g, "Mail & Arsch");
+
 	textNode.nodeValue = v;
 }
 
